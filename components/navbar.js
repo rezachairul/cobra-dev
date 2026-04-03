e89c5db9ff68f459dfa98b84d9787ccf1bc016e2
rezachairul <rezachairul6@gmail.com>
2026-04-03 21:49:19 +0700

    stable before styling experiment

--- components/navbar.js
+++ components/navbar.js
@@ -117,7 +117,7 @@ const Navbar = () => {
 
             {/* Hamburger */}
             <button onClick={() => setMenuOpen(!menuOpen)} className="md:hidden">
-              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-menu-2"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M4 6l16 0" /><path d="M4 12l16 0" /><path d="M4 18l16 0" /></svg>          
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="icclearon icon-tabler icons-tabler-outline icon-tabler-menu-2"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M4 6l16 0" /><path d="M4 12l16 0" /><path d="M4 18l16 0" /></svg>          
             </button>
 
           </div>
